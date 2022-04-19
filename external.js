61b608217c66f89c6162a3c554b228507e904296
NimarN <nimarnasir1128@gmail.com>
2022-04-19 11:01:05 -0400

    Created computerPlay function and playRound function within external.js

--- external.js
+++ external.js
@@ -1,3 +1,20 @@
 let random_int = Math.floor(Math.random() * 3);
 
-console.log(random_int);
+
+
+function computerPlay(){
+    let plays = ["Rock", "Paper", "Scissors"];
+    let play = plays[random_int];
+    return play;
+}
+
+function playRound(player_selection, computer_selection){
+    player_selection = prompt().toLowerCase()
+
+    console.log(player_selection);
+    
+}
+
+let computer_move = computerPlay()
+console.log(computer_move)
+playRound()

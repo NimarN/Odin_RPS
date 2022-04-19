b1ec5c066a35f16ec1a4de69a0a1bd065cc03229
NimarN <nimarnasir1128@gmail.com>
2022-04-19 11:08:21 -0400

    edited external.js function

--- external.js
+++ external.js
@@ -3,18 +3,18 @@ let random_int = Math.floor(Math.random() * 3);
 
 
 function computerPlay(){
-    let plays = ["Rock", "Paper", "Scissors"];
+    let plays = ["rock", "paper", "scissors"];
     let play = plays[random_int];
     return play;
 }
 
 function playRound(player_selection, computer_selection){
-    player_selection = prompt().toLowerCase()
 
     console.log(player_selection);
-    
+    console.log(computer_selection);
 }
 
-let computer_move = computerPlay()
-console.log(computer_move)
-playRound()
+
+const player_selection = prompt().toLowerCase()
+const computer_selection = computerPlay()
+console.log(playRound(player_selection, computer_selection));

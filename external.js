f29bca0d69f0d5dcac20fb5097d6bcbaf634affd
NimarN <nimarnasir1128@gmail.com>
2022-05-05 09:46:16 -0400

    completed script for game function

--- external.js
+++ external.js
@@ -9,12 +9,60 @@ function computerPlay(){
 }
 
 function playRound(player_selection, computer_selection){
+    console.log(`It is ${player_selection} vs ${computer_selection}`)
 
-    console.log(player_selection);
-    console.log(computer_selection);
+    if (player_selection == "rock" && computer_selection == "scissors"){
+        return "You Win";
+    } else if (player_selection == "paper" && computer_selection == 'rock'){
+        return "You Win";
+    } else if (player_selection == "scissors" && computer_selection == "paper"){
+        return "You Win";
+    } else if (player_selection == "rock" && computer_selection == "paper"){
+        return "You Lose";
+    } else if (player_selection == "paper" && computer_selection == "scissors"){
+        return "You Lose";
+    } else if (player_selection == "scissors" && computer_selection == "rock"){
+        return "You Lose";
+    } else{
+        return "Tie";
+    }
+}
+
+function game(){
+
+    let player_total = 0;
+    let cpu_total = 0;
+
+    for (let i = 0; i < 5; i++){
+        let player_selection = prompt("Pick rock/paper/scissors").toLowerCase();
+        let computer_selection = computerPlay();
+        let round = playRound(player_selection, computer_selection);
+        console.log(round)
+
+        if (round == "You Win"){
+            player_total ++;
+            console.log(`Player total is now ${player_total}`)
+        } else if (round == "You Lose"){
+            cpu_total ++;
+            console.log(`cpu total is now ${cpu_total}`)
+        }
+    }
+    if (cpu_total > player_total){
+        return "cpu wins the game!";
+    } else if (player_total > cpu_total){
+        return "you won the game"
+    } else {
+        return "the game was a tie!";
+    }
+
+    
 }
 
 
-const player_selection = prompt().toLowerCase()
-const computer_selection = computerPlay()
-console.log(playRound(player_selection, computer_selection));
+function test(){
+    for(let i = 0; i < 5; i++){
+        console.log(3)
+    }
+}
+
+console.log(game())
